fd4c5f59b5b6115ec4e0519ee79af756e154b067
AChar770 <amandatcharles@yahoo.com>
2026-03-17 11:32:36 -0400

    created a new database and new tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,19 @@
--- TODO
+DROP DATABASE IF EXISTS filez;
+CREATE DATABASE filez;
+
+CREATE TABLE folders(
+  ID SERIAL PRIMARY KEY,
+  name text UNIQUE NOT NULL
+);
+  
+CREATE TABLE files(  
+  ID SERIAL PRIMARY KEY,
+  name text NOT NULL,
+  size int NOT NULL,
+  folder_id int REFERENCES folders(id) ON DELETE CASCADE
+  CREATE UNIQUE INDEX ON files(name, folder_id)
+);
+
+-- INSERT INTO folders(name) VALUES ()
+--    INSERT INTO folders(name) VALUES ()
+-- )
